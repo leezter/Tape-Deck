3ada7372b4d0505682e168187aa9654feab77cab
leezter <66684146+leezter@users.noreply.github.com>
2025-05-09 20:45:40 +0200

    Added an SQLite Data Manager

--- data_manager_interface.py
+++ data_manager_interface.py
@@ -14,3 +14,31 @@ class DataManagerInterface(ABC):
         Retrieve all movies for a specific user by their ID.
         """
         pass
+
+    @abstractmethod
+    def add_user(self, user):
+        """
+        Add a new user to the data source.
+        """
+        pass
+
+    @abstractmethod
+    def add_movie(self, movie):
+        """
+        Add a new movie to the data source.
+        """
+        pass
+
+    @abstractmethod
+    def update_movie(self, movie):
+        """
+        Update the details of a specific movie in the data source.
+        """
+        pass
+
+    @abstractmethod
+    def delete_movie(self, movie_id):
+        """
+        Delete a specific movie from the data source.
+        """
+        pass
